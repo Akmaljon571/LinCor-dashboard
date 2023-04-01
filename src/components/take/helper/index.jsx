43c16c7a2c3baef9fec6170dddbc93481f5a5377
Akmaljon571 <ahmadjonovakmal079@gmail.com>
2023-04-02 01:41:15 +0500

    input for user_take

--- src/components/take/helper/index.jsx
+++ src/components/take/helper/index.jsx
@@ -1,8 +1,108 @@
+import { useState } from "react";
+import Tillar from "../../../languages/language"
+import useStart from "../../../hooks/useStart";
+import { apiGet, host } from "../../../utils/api";
+import useComponent from "../../../hooks/useComponent";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { message } from "antd";
+
 function TakeInput() {
+    const [user, setUser] = useState([])
+    const [course, setCourse] = useState([])
+    const { token, setCount, count } = useComponent()
+    const { lang } = useStart()
+    const findUser = useRef()
+    const findCourse = useRef()
+    const [messageApi, contextHolder] = message.useMessage()
+
+    
+    useEffect(() => {
+        apiGet('/users/statistika', token)
+        .then(re => re.json())
+        .then(data => setUser(data?.allUsers))
+    }, [count,token]);   
+    
+    useEffect(() => {
+        apiGet('/courses', token)
+        .then(re => re.json())
+        .then(data => setCourse(data))
+    }, [count, token]); 
+    
+    const send = () => {
+        const userId = findUser.current?.value
+        const courseId = findCourse.current?.value
+        const key = 'updatable'
+        messageApi.open({
+            key,
+            type: 'loading',
+            content: 'Loading...',
+        })
+        if (courseId && userId) {
+            console.log(courseId,
+                userId)
+            fetch(host + '/courses_open_users/create', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    autharization: token
+                },
+                body: JSON.stringify({
+                    courseId,
+                    userId
+                })
+            })
+            .then(re => re.json())
+            .then(data =>  {
+            if (data.status === 200) {
+                setCount(count + 1)
+                setTimeout(() => {
+                  messageApi.open({
+                    key,
+                    type: 'success',
+                    content: 'Loaded!',
+                    duration: 2,
+                  })
+                }, 1000)
+              } else {
+                setTimeout(() => {
+                  messageApi.open({
+                    key,
+                    type: 'error',
+                    content: 'User Already buy Course',
+                    duration: 2,
+                  })
+                }, 1000)
+              }
+        })} else {
+            setTimeout(() => {
+              messageApi.open({
+                key,
+                type: 'error',
+                content: 'Loaded!',
+                duration: 2,
+              })
+            }, 1000)
+        }
+    }
+
     return (
-        <>
-            Input
-        </>
+        <div className="take_input">
+            {contextHolder}
+            <div>
+                <select ref={findUser}>
+                    {user.map(e => (
+                        <option key={e?.user_id} value={e?.user_id}>{e.email}</option>
+                    ))}
+                </select>
+                <select ref={findCourse}>
+                    {course.map(e => (
+                        <option key={e?.course_id} value={e.course_id}>{e.course_title}</option>
+                    ))}
+                </select>
+            </div>
+            <button onClick={send}>{Tillar[0][lang].sent}</button>
+        </div>
     );
 }
 
